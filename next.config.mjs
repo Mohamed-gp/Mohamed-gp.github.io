5973dc20c1210bb838319baacf1eb956c8ba6ef0
Mohamed-gp <mohamedterba6@gmail.com>
2024-08-08 07:09:53 +0100

    setting up domain

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
   output: "export", // <=== enables static exports
   reactStrictMode: true,
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
