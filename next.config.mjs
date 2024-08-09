ad9ec1a3cefd7e0732afd55a85b932f1fc9f972f
Mohamed-gp <mohamedterba6@gmail.com>
2024-08-09 18:59:20 +0100

    remove static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // <=== enables static exports
-  reactStrictMode: true,
-  distDir: "dist",
-  images: {
-    unoptimized: true,
-  },
+  // output: "export", // <=== enables static exports
+  // reactStrictMode: true,
+  // distDir: "dist",
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
