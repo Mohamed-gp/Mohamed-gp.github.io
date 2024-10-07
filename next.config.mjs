3f2311becc62802d1d9579970358fab255f50c70
Mohamed-gp <mohamedterba6@gmail.com>
2024-10-07 11:39:32 +0100

    fix bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
-import withPWA from "next-pwa";
 
 const nextConfig = {
   // Your existing Next.js configuration
 };
 
-export default withPWA({
-  dest: "public",
-  // Additional PWA options can be added here
-})(nextConfig);
+export default nextConfig;
