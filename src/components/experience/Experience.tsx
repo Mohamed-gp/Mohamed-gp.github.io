c6ec72a65f247c725391da69ab81a96eb058db8b
Mohamed-gp <mohamedterba6@gmail.com>
2025-05-01 12:39:42 +0100

    fix dev dependencies

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -230,7 +230,18 @@ export default function Experience() {
                       </Badge>
                     </div>
                     <p className="text-primary font-medium text-sm sm:text-base flex items-center gap-2">
-                      {job.company}
+                      {job.company == "Fiverr" ? (
+                        <Link
+                          href={job.website}
+                          target="_blank"
+                          className="text-primary font-medium text-sm sm:text-base flex items-center gap-2"
+                        >
+                          {job.company}
+                        </Link>
+                      ) : (
+                        <>{job.company}</>
+                      )}
+
                       {job.website && (
                         <Link
                           href={job.website}
